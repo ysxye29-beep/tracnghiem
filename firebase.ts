86f1cf94dc6298a4902d4aced58b1e6e45e126d2
kili <ysxye29@gmail.com>
2026-03-27 09:48:43 +0700

    Refactor Firestore initialization in firebase.ts
    Removed firestoreDatabaseId from getFirestore initialization.

--- firebase.ts
+++ firebase.ts
@@ -5,7 +5,7 @@ import firebaseConfig from './firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
 
 export const loginWithGoogle = async () => {
